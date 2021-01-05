9e88f8c76b533fa6200e01350012d24efb68f9dd
Alexey Maiorov <alexey.maiorov@utc.com>
2021-01-04 23:21:15 -0500

    stub out test

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import styles from './Test.module.scss';
 
 const Test = (props) => {
 
+  let [slide, setSlide] = useState(0);
   useEffect((props) => {
     console.log('take the test');
-  })
+  });
+
+  const rotate = () => {
+    console.log(slide)
+    setSlide(slide + 1 === 3 ? 0 : slide + 1);
+    console.log(slide)
+  }
 
   return (
-    <section className={ cn('dark', styles.wrapper) }>
+    <section className={ styles.wrapper }>
       <div className="content">
         <h1>What Can I Expect?</h1>
         <h3>Take the Test!</h3>
-        <p>Answer two simple questions to find out how much is your potential additional income and how Astute Subprime Lending can help your business.</p>
-        <ul>
+        <p>Answer two simple questions to find out how much is your potential additional income<br /> and how Astute Subprime Lending can help your business.</p>
+        <ul onClick={ rotate } data-current-slide={ slide }>
           <li>
             <span className={ styles.title }>Q1: How many customers do you lose because of credit per month?</span>
             <button>5 customers</button>
@@ -32,7 +39,6 @@ const Test = (props) => {
           </li>
           <li>
             <span className={ styles.title }>Learn your possible additional income.</span>
-            <p>Learn your possible additional income.</p>
             <span className={ styles.title }>
               If you capture half, you potential additional income is<br /><b>$32,000</b> per month
               <br />
