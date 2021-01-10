52e06e4112dcbe6caff48ea07a45aa91286742b9
Alexey Maiorov <alexey.maiorov@utc.com>
2021-01-10 17:36:25 -0500

    add error checking

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 
 import styles from './Contact.module.scss';
 
 const Contact = (props) => {
 
+  let [errorCount, setErrorCount] = useState();
+
   useEffect((props) => {
     console.log('contact');
-  })
+  }, [errorCount]);
+
+  const checkForm = (evt) => {
+    let scrolled = false;
+    let errors = 0;
+    evt.preventDefault();
+    document.querySelectorAll('[required]').forEach((item, i) => {
+      if (!item.value) {
+        item.classList.add(styles.error);
+        errors++;
+        if (!scrolled) {
+          item.nextSibling.scrollIntoView({ behavior: 'smooth' });
+          scrolled = true;
+        }
+      } else {
+        item.classList.remove(styles.error);
+      }
+    });
+    if (errors === 0) {
+      document.querySelector('form').classList.add(styles.collapsed);
+    }
+    setErrorCount(errors);
+  };
 
   return (
     <section className={ styles.wrapper } id="contact">
       <div className="content">
         <h1>Contact Us</h1>
-        <p>If you have any question about Astute Subprime Consulting, feel free to ask us using the form below.<br />We will get back to you within one business day.</p>
+        { (typeof errorCount === 'undefined' || errorCount > 0) &&
+          <>
+            <p>If you have any question about Astute Subprime Consulting, feel free to ask us using the form below.<br />We will get back to you within one business day.</p>
+          </>
+        }
+        { typeof errorCount !== 'undefined' && errorCount === 0 &&
+          <>
+            <h2>Thank you for contacting us!</h2>
+            <p>We will get back to you within one business day.</p>
+          </>
+        }
         <form>
           <div className={ styles.grid }>
             <div className={ styles.col }>
@@ -59,7 +93,7 @@ const Contact = (props) => {
               <label htmlFor="inquiry">Inquiry</label>
             </div>
           </div>
-          <a href="#send" className={ cn('button', 'dark') }>Send</a>
+          <a href="#send" className={ cn('button', 'dark') } onClick={ checkForm }>Send</a>
         </form>
       </div>
     </section>
